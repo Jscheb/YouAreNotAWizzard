3c3382cf4dd1e6e19ad374157de54d81479fea86
Laurenz Dilba <laurenz.dilba@hotmail.de>
2021-04-26 23:50:17 +0200

    Limited FPS

---
+++ YouAreNotAWizard/Assets/Scripts/Core/Setup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour
+{
+    int frameCount = 0;
+    float dt = 0.0f;
+    float fps = 0.0f;
+    float updateRate = 2.0f; // updates per second
+     
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 140;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount++;
+        dt += Time.deltaTime;
+        if (dt > 1.0f/updateRate)
+        {
+            fps = frameCount / dt ;
+            frameCount = 0;
+            dt -= 1.0f/updateRate;
+            Debug.Log((int)fps + " fps");
+        }
+        
+        
+    }
+}
