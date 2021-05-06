e217b85a1afb3102ebe36e303b686c275f80178b
Christian Hendricks <Christian.hendricks1@gmx.de>
2021-05-06 23:01:19 +0200

    Damage mit Timer

--- YouAreNotAWizard/Assets/ChrisselSchissel/Scripts/DetectionScript.cs
+++ YouAreNotAWizard/Assets/ChrisselSchissel/Scripts/DetectionScript.cs
@@ -5,36 +5,32 @@ using UnityEngine;
 public class DetectionScript : MonoBehaviour
 {
     public float damageTimer;
-    private float currentTimer;
+    private float currentTimer = 0f;
     void Update()
     {
-        Timer();
-    }
-    public void DestroyHitBox()
-    {
-        Destroy(gameObject);
+        
     }
     private void OnTriggerStay(Collider other)
     {
         //Physics.IgnoreCollision(GetComponent<BoxCollider>(), transform.GetComponentInParent<CharacterController>());
         //Debug.Log(other.name);
+        bool damageApply = Timer();
         Enemy enemy = other.GetComponent<Enemy>();
-        if (enemy != null)
+        if (enemy != null && damageApply)
         {
-            enemy.TakeDamage(20);
             Debug.Log("Funzt sehr gut");
             ApplyDamage(enemy);
         }
     }
     bool Timer()
     {
-        if(currentTimer > 0)
+        if (currentTimer > 0)
         {
             currentTimer -= Time.deltaTime;
         }
-        if(currentTimer <= 0)
+        if (currentTimer <= 0)
         {
-            currentTimer = 5;
+            currentTimer = damageTimer;
             Debug.Log("Timer funktioniert: " + currentTimer);
             return true;
         }
@@ -44,4 +40,9 @@ public class DetectionScript : MonoBehaviour
     {
         enemy.TakeDamage(20);
     }
+    public void DestroyHitBox()
+    {
+        Destroy(gameObject);
+    }
+    
 }
