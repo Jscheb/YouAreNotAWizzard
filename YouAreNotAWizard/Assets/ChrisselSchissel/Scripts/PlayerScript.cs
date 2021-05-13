c45c820b4f9b899a9ca3827ed39ab9bdcdb94e20
Christian Hendricks <Christian.hendricks1@gmx.de>
2021-05-13 21:34:35 +0200

    FlameSpell hinzugefügt und Renderer der Hitbox ausgemacht

--- YouAreNotAWizard/Assets/ChrisselSchissel/Scripts/PlayerScript.cs
+++ YouAreNotAWizard/Assets/ChrisselSchissel/Scripts/PlayerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -8,10 +9,12 @@ public class PlayerScript : MonoBehaviour
     public GameObject hitBox;
     public DetectionScript realOne;
     private DetectionScript clone;
+    public VisualEffect flamespell;
 
     void Start()
     {
         cam = Camera.main;
+        flamespell.Stop();
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class PlayerScript : MonoBehaviour
     }
     void HitBox()
     {
+        //Get Raycast at a certain location
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         Vector3 direction = Vector3.up.normalized;
@@ -35,18 +39,23 @@ public class PlayerScript : MonoBehaviour
         {
             Debug.Log("Yes");   
             clone = Instantiate(realOne, transform.GetChild(0).position + direction*2, directionAngle);
+            flamespell.Play();
+
         }
         if (Input.GetKey(KeyCode.Mouse0))
         {
             //direction = (hit.point - transform.position).normalized;
             direction = new Vector3(direction.x, 0.0f, direction.z);
-            clone.transform.position = transform.GetChild(0).position + direction * clone.transform.localScale.z;
+            clone.transform.position = transform.GetChild(0).position + direction * 0.6f * clone.transform.localScale.z;
+            flamespell.transform.position = transform.GetChild(0).position + direction * 2;
             directionAngle = Quaternion.LookRotation(direction);
             clone.transform.rotation = directionAngle;
+            flamespell.transform.rotation = directionAngle;
         }
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             clone.DestroyHitBox();
+            flamespell.Stop();
         }
     }
 }
