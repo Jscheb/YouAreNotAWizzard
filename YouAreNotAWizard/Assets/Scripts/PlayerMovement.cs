02fe3d880b138f290e39d33dee954fd362cad864
Laurenz Dilba <laurenz.dilba@hotmail.de>
2021-06-14 12:13:41 +0200

    Added Walking script to player, Animation & Gravity (graphics improvements)

--- YouAreNotAWizard/Assets/Scripts/PlayerMovement.cs
+++ YouAreNotAWizard/Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,18 @@ public class PlayerMovement : MonoBehaviour
     public float turnSpeed;
     public float turnSmoothTime;
     public Transform cam;
+    private float playerGravity;
 
     void Update()
     {
         MoveThePlayer();
+        ApplyGravity();
+    }
+
+    void ApplyGravity()
+    {
+        Vector3 gravityVector = Quaternion.Euler(0f, playerGravity, 0f) * Vector3.down;
+        controller.Move(gravityVector.normalized * Time.deltaTime);
     }
     void MoveThePlayer()
     {
@@ -26,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+            controller.Move(moveDir.normalized * (movementSpeed * Time.deltaTime));
         }
     }
 }
