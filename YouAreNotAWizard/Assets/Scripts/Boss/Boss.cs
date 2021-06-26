9755745e1a88cfa8ecd7f6edefca3f5269c870a5
Jan <jan.scheben@gmail.com>
2021-06-26 22:09:00 +0200

    HealthBar 1.0

---
+++ YouAreNotAWizard/Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : Enemy
+{
+    public Slider healthBar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = health;
+    }
+   
+   
+}
